74dd115d6f596abb5a6d634ef0c5fac643857360
pmanlukas <lukas.pollmann@tum.de>
2018-06-19 11:39:18 +0200

    started reimplementation

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # tempfaker
-A Python script to create fake temperature values and publish them to MQTT. Also there is a file to publish sensor data of the adafruit BME280 to MQTT
+
+### under maintainence - the structure and functions are going to change
+
+A Python script to create fake temperature values and publish them to MQTT. Also there is a file to publish sensor data of the adafruit BME280 to MQTT.
+
+
---
+++ sensor/main.py
@@ -0,0 +1,23 @@
+#TODO: reimplement the code as objects
+import time
+import json
+import paho.mqtt.client as mqtt
+
+
+#class that implements a fake sensor, that can deliver various temp data
+class sensor():
+
+    def __init__(self):
+        self. interval = 0.1
+        self.isMetric = True
+        
+    def get_temp():
+
+    def get_pressure():
+
+    def set_interval(interval):
+    
+    def set_format(isMetric):
+
+#class that implements a mqtt client, to send the sensor data
+class mqtt_client():
